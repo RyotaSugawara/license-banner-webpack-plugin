f6a490edc5b588204b13430eb63f68cc08da36d0
RyotaSugawara <ryo.suga.dev@gmail.com>
2017-01-31 19:45:37 +0900

    test: add test code

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ class LicenseBannerWebpackPlugin {
                 if (banner)
                   chunk.files.forEach(file => {
                     compilation.assets[file] = new ConcatSource(
-                      `/*\n${banner}\n */\n`,
+                      `/*\n${banner}\n*/\n`,
                       compilation.assets[file]
                     );
                   });
